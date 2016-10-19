16190d26b71535e36ce509043d4493515c4b22c8
Leblanc-Gervais Maxime <1258442@decinfo.cvm>
2016-10-19 12:42:29 -0400

    ajout oto et planif

--- oto/OTO_cerveau.py
+++ oto/OTO_cerveau.py
@@ -2,10 +2,23 @@ import bge
 
 
 class Cerveau():
-    def __init__(self,parent): # parent est l'objet participant qui vous represente
+    def __init__(self,parent): # parent est l'objet modele qui vous represente
         self.analyseur=Analyseur()
-        
+        self.modele = parent # Référence au modele qui vous represente
+        self.participant=self.modele.moi # Référence au participant qui vous represente
+        self.auto = self.participant.auto # Référence a l'auto du participant qui vous represente
+       
+       
+
 class Analyseur():
     def __init__(self):
         self.operations_recentes=[]
 
+
+class AnalyseLaser():
+    def __init__(self):
+    	pass
+
+class AnalyseImage():
+	def __init__(self):
+		pass
