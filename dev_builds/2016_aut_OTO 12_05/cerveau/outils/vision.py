07b6c7522e847eba69274043589ca18f2ad8a018
Evan Hutchinson <evan.brosky@gmail.com>
2016-12-07 08:27:25 -0500

    Ajouté les fonctions d'analyse d'image supplémentaires dans vision.py

--- dev_builds/2016_aut_OTO 12_05/cerveau/outils/vision.py
+++ dev_builds/2016_aut_OTO 12_05/cerveau/outils/vision.py
@@ -9,16 +9,71 @@ class Vision():
         self.modele = parent # Référence au modele qui vous represente
     
     def traitementImage(self):
-        pass
-        #print("analyseImage!")
+        print("analyseImage!")
 
     #prend UN screenshot
     def prendreScreenshot(self, nom):
         bge.render.makeScreenshot(bge.ppath+"images/"+nom)
 
+    #prend un screenshot, le nomme temp, et va le chargen en objet PIL
+    #retourne le screenshot sous forme d'object PIL
     def getScreen(self):
         prendreScreenshot("temp")
-        image = Image.open("images")
+        image = Image.open("images/temp")
+        return image
+
+    #retourne true si une rue à droite est détectée
+    #A utiliser avec une image monochrome! pas RGB!
+    def rueADroite(image):
+        avantNoir = False
+        entreNoir = False
+        apresNoir = False
+        width, height = image.size
+        ligneDroite = (width-2, 0, width-1, height-2)
+        image = image.crop(ligneDroite)
+
+        for i in range(height-2):
+            pixel = image.getpixel((0, i))
+            if pixel < 20:
+                avantNoir = True
+            if avantNoir is True:
+                if pixel == 255:
+                    entreNoir = True
+            if avantNoir is True and entreNoir is True:
+                if pixel < 20:
+                    apresNoir = True
+
+        return avantNoir and entreNoir and apresNoir
+
+    #retourne True s'il y a un virage à gauche possible
+    #a utiliser avec des images monochromes! pas RGB!
+    def rueAGauche(image):
+        avantNoir = False
+        entreNoir = False
+        apresNoir = False
+        width, height = image.size
+        ligneGauche = (0, 0, 1, height-2)
+        image = image.crop(ligneGauche)
+
+        for i in range(height-2):
+            pixel = image.getpixel((0, i))
+            if pixel < 20:
+                avantNoir = True
+            if avantNoir is True:
+                if pixel == 255:
+                    entreNoir = True
+            if avantNoir is True and entreNoir is True:
+                if pixel < 20:
+                    apresNoir = True
+
+        return avantNoir and entreNoir and apresNoir
+
+    def convertirBoW(image):
+        imageBoW = Image.new('L', image.size) #cree une nouvelle image grayscale
+        imageBoW.paste(image) #colle le contenu de l'image rgb
+        imageBoW = imageBoW.point(lambda x: 0 if x<50 else 255, '1') #converti en noir OU blanc
+        return imageBoW #retourne l'objet PIL monochrome
+
 
     
     # Retourne une string clarifiant la section de l'image en HORIZ_VERTI ex : CG_C (Centre Gauche en Horizontal et Centre Vertical)
@@ -28,7 +83,7 @@ class Vision():
         baseY = height/3
         result = []
 
-        # Trouver section Horizontal
+        # Trouver section Horizontal 
         for i in range(0,5):
             if x < baseX*(i+1) and x >= baseX*i:
                 #0 : Gauche
@@ -62,6 +117,21 @@ class Vision():
 
         return result
 
+        # Append les points X, en relation avec un Y initial, qui ont un changement de couleur dans une liste
+    def changementCouleurHorizontal(image,y):
+        width, height = image.size
+        derniereCouleur = image.getpixel((0,y))
+        result=[]
+
+        for i in range(0,width):
+                if not isCouleursSimilaires(derniereCouleur, image.getpixel((i,y)), 5): # Si la derniere couleur est differente de la nouvelle
+                    result.append(i)
+
+                derniereCouleur = image.getpixel((i,y))
+            
+
+        return result
+
     # Cree matrice avec points qui definit un rectangle
     def defRect(image):
         width, height = image.size
@@ -82,5 +152,82 @@ class Vision():
 
         image.save('out.png') ## TEST POUR FEEDBACK VISUEL
         #image.show() ## TEST POUR FEEDBACK VISUEL
+
+        return arrayRect
+
+    def trouvePancarte(image):
+        width, height = image.size
+        partieDroite = image.crop(width/2, 0, width, height)
+        partieDroite.show()
+
+    def isCouleursSimilaires(pixelA, pixelB, threshold):
+        r1,g1,b1 = pixelA
+        r2,g2,b2 = pixelB
+        result = False
+
+        if abs(r1-r2) <= threshold:
+            if abs(g1-g2) <= threshold:
+                if abs(b1-b2) <= threshold:
+                    result = True
+
+        return result
+
+    def getCouleurString(pixel): ######################################################## A TESTER
+        r,g,b = pixel
+        couleur = "gris"
+
+        # Rouge
+        if r > g and r > b and b is g:
+            couleur = "rouge"
+
+        # Jaune
+        elif g > r and b > r:
+            couleur = "jaune"
+
+        # Rose
+        elif r > b and b > g:
+            couleur = "rose"
+
+        # Vert
+        elif g > r and g > b and r is b:
+            couleur = "vert"
+
+        # Aqua
+        elif b > g and g > r:
+            couleur = "aqua"
+
+        # Bleu
+        elif b > r and b > r and r is g:
+            couleur = "bleu"
+
+        # Blanc
+        elif r > 200 and g > 200 and b > 200:
+            couleur = "blanc"
+
+        return couleur
+
+# Cree matrice avec points qui definit un rectangle
+    def dessiner(image):
+        width, height = image.size
+        arrayRect = []
+
+        for i in range(0,width,1):
+            arrayVertical = changementCouleurVertical(image,i)
+            tuple_rgb = (0,0,255) ## TUPLE COULEUR ROUGE POUR FEEDBACK VISUEL
+            
+            for j in enumerate(arrayVertical):
+                tuplePosition = (i,j[1])
+                image.putpixel(tuplePosition,tuple_rgb)
+
+        for i in range(0,height,1):
+            arrayVertical = changementCouleurHorizontal(image,i)
+            tuple_rgb = (0,0,255) ## TUPLE COULEUR ROUGE POUR FEEDBACK VISUEL
+            
+            for j in enumerate(arrayVertical):
+                tuplePosition = (j[1],i)
+                image.putpixel(tuplePosition,tuple_rgb)
+
+        #image.save('out.png') ## TEST POUR FEEDBACK VISUEL
+        image.show() ## TEST POUR FEEDBACK VISUEL
 
         return arrayRect
